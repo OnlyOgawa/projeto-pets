8b3bab985abe28c7efc9e2c19ceb04a5375f06cb
onlyogawa <gustavo.ogawa97@gmail.com>
2025-10-15 16:15:01 -0300

    requisicao para pet cadastrado e exibindo na dashboard

--- frontend/src/components/pages/Pet/AddPet.js
+++ frontend/src/components/pages/Pet/AddPet.js
@@ -2,8 +2,8 @@ import api from '../../../utils/api'
 
 import styles from './AddPet.module.css'
 
-import {useState} from 'react'
-import{useHistory} from 'react-router-dom'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 /* components */
 import PetForm from '../../form/PetForm'
@@ -12,13 +12,53 @@ import PetForm from '../../form/PetForm'
 import useFlashMessage from '../../../hooks/useFlashMessage'
 
 function AddPet() {
+    const [token] = useState(localStorage.getItem('token') || '')
+    const { setFlashMessage} = useFlashMessage()
+    const navigate = useNavigate()
+
+    async function registerPet(pet) {
+        let msgType = 'success'
+
+        const formData = new FormData()
+
+        Object.keys(pet).forEach((key) => {
+            if (key === 'images') {
+                for (let i = 0; i < pet[key].length; i++) {
+                    formData.append('images', pet[key][i])
+                }
+            } else {
+                formData.append(key, pet[key])
+            }
+        })
+
+        try {
+            const response = await api.post('pets/create', formData, {
+                headers: {
+                    Authorization: `Bearer ${JSON.parse(token)}`,
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+
+            const data = response.data
+
+            setFlashMessage(data.message, msgType)
+
+            setTimeout(() => {
+                navigate('/pets/mypets')
+            }, 2000)
+        } catch (err) {
+            msgType = 'error'
+            setFlashMessage(err.response?.data?.message || 'Erro ao cadastrar o pet.', msgType)
+        }
+    }
+
     return(
         <section className={styles.addpet_header}>
             <div>
                 <h1>Cadastre um pet</h1>
                 <p>Depois ele ficara disponivel para adocao</p>
             </div>
-            <PetForm btnText='Cadastrar Pet' /> 
+            <PetForm handleSubmit={registerPet} btnText='Cadastrar Pet' /> 
         </section>
     )
 }
--- frontend/src/components/pages/Pet/MyPets.js
+++ frontend/src/components/pages/Pet/MyPets.js
@@ -1,8 +1,32 @@
+import api from '../../../utils/api'
+
+import styles from './Dashboard.module.css'
+
 import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 
+import RoundedImage from '../../layout/RoundedImage'
+
+/* hooks */
+import useFlashMessage from '../../../hooks/useFlashMessage'
+
+
 function MyPets() {
     const [pets, setPets] = useState([])
+    const [token] = useState(localStorage.getItem('token') || '')
+    const {setFlashMessage} = useFlashMessage()
+
+    useEffect(() => {
+        api.get('pets/mypets', {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(token)}`
+            }
+        })
+        .then((response) => {
+            setPets(response.data.pets)
+        })
+    }, [token])
+
     return(
         <section>
             <div>
@@ -10,11 +34,29 @@ function MyPets() {
                 <Link to="/pet/add">Cadastrar Pet</Link>
             </div>
             <div>
-                {pets.lenght > 0 && 
-                <p>Meus Pets cadastrados</p>
+                {pets.length > 0 && 
+                    pets.map((pet) => (
+                        <div key={pet._id}>
+                            <RoundedImage 
+                                src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`}
+                                alt={pet.name}
+                                width='75px'
+                            />
+                            <span className='bold'>{pet.name}</span>
+                            <div className={styles.action}>
+                                {pet.available ? (
+                                <>
+                                    {pet.adopter && <button>Concluir adocao</button>}
+                                    <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
+                                    <button>Excluir</button>
+                                </>
+                                ) : (
+                                 <p>Pet ja adotado</p>)}
+                            </div>
+                        </div>
+                    ))
                 }
-                {pets.length === 0 &&
-                <p>Nao ha Pets cadastrados</p>}
+                {pets.length === 0 && <p>Nao ha Pets cadastrados</p>}
             </div>
         </section>
     )
