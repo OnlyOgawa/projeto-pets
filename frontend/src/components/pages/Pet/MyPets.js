1dd2d362f9b27189c9863322cd6e0e8e4f904e30
onlyogawa <gustavo.ogawa97@gmail.com>
2025-10-20 15:19:46 -0300

    CSS dashboard/ remocao e edicao de pets

--- frontend/src/components/pages/Pet/MyPets.js
+++ frontend/src/components/pages/Pet/MyPets.js
@@ -27,28 +27,51 @@ function MyPets() {
         })
     }, [token])
 
+    async function removePet(id) {
+        let msgType = 'success'
+
+        const data = await api.delete(`/pets/${id}`, {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(token)}`
+            }
+        }).then((response) => {
+            const updatedPets = pets.filter((pet) => pet._id !== id)
+            setPets(updatedPets)
+            return response.data
+        })
+        .catch((err) => {
+            msgType = 'error'
+            return err.response.data
+        })
+
+        setFlashMessage(data.message, msgType)
+    }
+
     return(
         <section>
-            <div>
+            <div className={styles.petlist_header}>
                 <h1>MyPets</h1>
                 <Link to="/pet/add">Cadastrar Pet</Link>
             </div>
-            <div>
+            <div className={styles.petlist_container}>
                 {pets.length > 0 && 
                     pets.map((pet) => (
-                        <div key={pet._id}>
+                        <div className={styles.petlist_row} key={pet._id}>
                             <RoundedImage 
                                 src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`}
                                 alt={pet.name}
                                 width='75px'
                             />
                             <span className='bold'>{pet.name}</span>
-                            <div className={styles.action}>
+                            <div className={styles.actions}>
                                 {pet.available ? (
                                 <>
-                                    {pet.adopter && <button>Concluir adocao</button>}
+                                    {pet.adopter && <button className={styles.conclude_btn}>Concluir adocao</button>}
                                     <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
-                                    <button>Excluir</button>
+                                    <button onClick={() =>{
+                                        removePet(pet._id)
+                                    }}
+                                    >Excluir</button>
                                 </>
                                 ) : (
                                  <p>Pet ja adotado</p>)}
